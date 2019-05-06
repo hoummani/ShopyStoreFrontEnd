2893e405ba1fef4884073955d9bed7a979227727
Anonymous <anonymous@localhost.localdomain>
2019-05-06 17:09:33 +0000

    set up login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './404/error/error.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent, pathMatch:'full'},
+
+  {path:'login', component:LoginComponent, pathMatch:'full'},
+  {path:'register', component:RegisterComponent, pathMatch:'full'},
+
+  {path:'**', component:ErrorComponent, pathMatch:'full'},
 
 ];
 
