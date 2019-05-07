b641ce595769df220ad5f67d5b8d1db93b32600e
Anonymous <anonymous@localhost.localdomain>
2019-05-07 03:41:11 +0000

    set up register component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,6 +12,10 @@ export class RegisterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(() => {
+      $('select').formSelect();
+
+    });
   }
 
 }
